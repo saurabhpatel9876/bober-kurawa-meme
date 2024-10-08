6247e706fe3d0d21d75783122ae8a43cc5192ce7
saurabh patel <saurabhpatel5061456@gmail.com>
2024-10-08 18:11:01 +0530

    5th

--- src/app/BoberMore/page.js
+++ src/app/BoberMore/page.js
@@ -1,13 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
+import bober from "@/assets/bober_with_men.webp"
 
 const page = () => {
   return (
     <>
-    <div
+    <div className="bg-yellow-900 flex-col">
+   <div
    className=" text-3xl mt-10 text-center text-neutral-500 max-w-4xl">
-     The "bober kurwa" meme originated from a series of humorous videos featuring Polish men enthusiastically recording beavers while exclaiming "Kurwa Bóbr," which translates to "f**k, a beaver." The earliest instances of this phrase can be traced back to YouTube videos from 2012 and 2014, where individuals would film beavers and use the phrase in an excited or comedic context23.
-     The meme gained significant traction around 2017, particularly with a viral video by YouTuber Fred Pierwszy Wielki, where he humorously chased a beaver, leading to widespread sharing on platforms like iFunny in 2018. By 2023, the meme had further spread to TikTok, where various adaptations and remixes garnered millions of views, solidifying its place in contemporary internet culture
+     "Bober kurwa" is a phrase that combines two Polish words: "bober," meaning "beaver," and "kurwa," a vulgar term that translates to "fk"** or "bitch." The phrase can be interpreted as a humorous or crude expression, often used in memes or internet videos, particularly those featuring people excitedly encountering beavers while using expletives.
+      
    </div>
+      <div>
+      <Image
+      src={bober}
+      width={300}
+      height={300}
+      alt="Picture of the author"
+    />
+      </div>
+    </div>
     </>
   )
 }
